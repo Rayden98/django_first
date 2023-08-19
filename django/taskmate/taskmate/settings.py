621d7f24c72ef31aa08156442a81e99653582b19
Enmanuel Cruz <rember98cjs@gmail.com>
2023-08-19 11:20:39 -0600

    another DIR fixed

--- django/taskmate/taskmate/settings.py
+++ django/taskmate/taskmate/settings.py
@@ -135,9 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-            '/Users/CJ/Desktop/django/django/taskmate/static/',
-        ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
