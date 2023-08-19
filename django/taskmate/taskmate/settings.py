098b5dd8cd122b706f4cb41923bf6ae37abe4dbc
Enmanuel Cruz <rember98cjs@gmail.com>
2023-08-19 11:57:59 -0600

    update https

--- django/taskmate/taskmate/settings.py
+++ django/taskmate/taskmate/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localgost", "127.0.0.1", "djangofirst-production.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['djangofirst-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://djangofirst-production.up.railway.app']
 
 
 # Application definition
